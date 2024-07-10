19b0035f881e86ce7a0b6bfe8803aa1eefbebc21
kervindholah <kervin92310@hotmail.fr>
2024-07-10 16:01:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project_directives';
+  title = 'Les directives';
+  title2 = 'Bonjour !';
+
+  isAdmin = true;
+
+  view () {
+
+  }
+
+  edit () {
+
+  }
+
+  toggleAdmin() {
+    this.isAdmin = !this.isAdmin;
+  }
+
 }
